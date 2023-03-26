2dc3ef1e31180fc0f56a37da11b5322e014b4cf7
ivan pronin <ivan@DESKTOP-IB7HMA3>
2023-03-26 10:01:50 +0300

    add game-even

---
+++ src/Even.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BrainGames\even;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use function cli\line;
+use function cli\prompt;
+
+function iseven($n)
+{
+    return $n % 2 === 0;
+}
+function gameEven()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $count = 0;
+    while ($count < 3) {
+        $randomNumber = rand(1, 100);
+        line("Question: $randomNumber");
+        $answer = prompt('Your answer: ');
+        if (iseven($randomNumber) === true) {
+            if ($answer === 'yes') {
+                $count = $count + 1;
+                line('Correct!');
+            } else {
+                line("'$answer' is wrong answer ;(. Correct answer was 'yes'.");
+                line("Let's try again,", $name, "!");
+                return;
+            }
+        }
+        if (iseven($randomNumber) === false) {
+            if ($answer === 'no') {
+                $count++;
+                line('Correct!');
+            } else {
+                line("'$answer' is wrong answer ;(. Correct answer was 'no'.");
+                line("Let's try again,", $name, "!");
+                return;
+            }
+        }
+    }
+    line("Congratulations,", $name, "!");
+}
