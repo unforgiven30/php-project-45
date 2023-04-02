7dea34d1fd4023b7c67bd696cb0d624e19b63a4c
ivan pronin <ivan@DESKTOP-IB7HMA3>
2023-04-03 00:21:52 +0300

    red Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -13,7 +13,7 @@ use function cli\line;
 use function cli\prompt;
 function hello()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
